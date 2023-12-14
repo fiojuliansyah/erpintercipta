1d26e11a347790ab5bdda6d7a2ba965e20630c31
Fio Juliansyah <fiojuliansyah@gmail.com>
2023-12-14 11:17:26 +0700

    edit

--- resources/views/mobiles/register-profile.blade.php
+++ resources/views/mobiles/register-profile.blade.php
@@ -80,7 +80,7 @@
             </a>
             <a href="#" data-menu="instant-3" class="card card-style" data-card-height="150">
                 <div class="card-center">
-                    <p class="color-white pl-3 mt-n2 mb-0 opacity-60">Dengan mengisi Formulir diatas, saya menyatakan bahwa Pengrekrutan di Intercipta Corporation dengan tegas tidak dipungut Biaya Apapun</p>
+                    <p class="pl-3 mt-n2 mb-0 opacity-60">Dengan mengisi Formulir diatas, saya menyatakan bahwa Pengrekrutan di Intercipta Corporation dengan tegas tidak dipungut Biaya Apapun</p>
                 </div>
                 <span class="badges bg-danger" style="text-align: center">Pernyataan</span>
             </a>
