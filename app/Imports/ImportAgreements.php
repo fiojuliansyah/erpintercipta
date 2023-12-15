f84c9344d53adc55b3c35a9009bb5a2aabad4e75
Fio Juliansyah <fiojuliansyah@gmail.com>
2023-12-15 11:25:44 +0700

    update agreement

--- app/Imports/ImportAgreements.php
+++ app/Imports/ImportAgreements.php
@@ -15,27 +15,27 @@ class ImportAgreements implements ToModel, WithStartRow
     {
         // Buat agreement baru
         $agreement = new Agreement([
-            'department' => $row[2],
-            'title' => $row[3],
-            'romawi' => $row[4],
-            'year' => $row[5],
-            'area' => $row[6],
-            'start_date' => Date::excelToDateTimeObject($row[7])->format('Y-m-d'),
-            'end_date' => Date::excelToDateTimeObject($row[8])->format('Y-m-d'),
-            'length_of_work' => $row[9],
-            'place' => $row[10],
-            'responsible' => $row[11],
-            'salary' => $row[12],
-            'department_allowance' => $row[13],
-            'attendance_allowance' => $row[14],
-            'comunication_allowance' => $row[15],
-            'beauty_allowance' => $row[16],
-            'food_allowance' => $row[17],
-            'transport_allowance' => $row[18],
-            'location_allowance' => $row[19],
-            'other_non_fix_allowance' => $row[20],
-            'penalty' => $row[21],
-            'addendum_id' => $row[22],
+            'department' => $row[3],
+            'title' => $row[4],
+            'romawi' => $row[5],
+            'year' => $row[6],
+            'area' => $row[7],
+            'start_date' => Date::excelToDateTimeObject($row[8])->format('Y-m-d'),
+            'end_date' => Date::excelToDateTimeObject($row[9])->format('Y-m-d'),
+            'length_of_work' => $row[10],
+            'place' => $row[11],
+            'responsible' => $row[12],
+            'salary' => $row[13],
+            'department_allowance' => $row[14],
+            'attendance_allowance' => $row[15],
+            'comunication_allowance' => $row[16],
+            'beauty_allowance' => $row[17],
+            'food_allowance' => $row[18],
+            'transport_allowance' => $row[19],
+            'location_allowance' => $row[20],
+            'other_non_fix_allowance' => $row[21],
+            'penalty' => $row[22],
+            'addendum_id' => $row[23],
             // ... tambahkan pemetaan lain sesuai kebutuhan
         ]);
 
@@ -48,8 +48,8 @@ class ImportAgreements implements ToModel, WithStartRow
         // Sekarang, buat atau asosiasikan Pkwt
         $pkwt = new Pkwt([
             'agreement_id' => $agreement->id,
-            'user_id' => $user_id,
-            'pkwt_number' => $row[1]
+            'user_id' => $row[0],
+            'pkwt_number' => $row[2]
             // ... tambahkan pemetaan lain sesuai kebutuhan
         ]);
 
