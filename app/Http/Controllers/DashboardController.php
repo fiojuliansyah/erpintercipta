81bc347dc55d1142faa698a4abd32ccd6a072aed
Fio Juliansyah <fiojuliansyah@gmail.com>
2024-01-17 17:37:51 +0700

    Update DashboardController.php

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -46,7 +46,7 @@ class DashboardController extends Controller
         $companies = Company::all();
 
         if ($agent->isMobile()) {
-            return view('dekstop.job-portal' ,compact('careers','user','allcareer','alluser','companies'));
+            return view('desktop.job-portal' ,compact('careers','user','allcareer','alluser','companies'));
         } elseif ($agent->isDesktop()) {
             return view('desktop.job-portal' ,compact('careers','user','allcareer','alluser','companies'));
         } else {
