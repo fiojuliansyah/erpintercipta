6fc458a8e5d8d18a416bc068ea6a10969d408f51
Fio Juliansyah <fiojuliansyah@gmail.com>
2024-06-10 09:47:20 +0700

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,6 +6,7 @@ use App\Models\Career;
 use App\Models\Product;
 use Jenssegers\Agent\Agent;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
