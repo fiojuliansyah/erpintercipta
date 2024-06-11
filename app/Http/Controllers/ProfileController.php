30e070b631b2953952fa3ad552585b9f99904bbc
Fio Juliansyah <fiojuliansyah@gmail.com>
2024-06-11 10:02:50 +0700

    Update ProfileController.php

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -41,25 +41,14 @@ class ProfileController extends Controller
         $user = Auth::user();
     
         $existingAvatar = $user->profile->avatar ?? null;
-        $existingCardKTP = $user->profile->card_ktp ?? null;
-        $existingCardIjazah = $user->profile->card_ijazah ?? null;
-        $existingCardSKCK = $user->profile->card_skck ?? null;
-        $existingCardCertificate = $user->profile->card_certificate ?? null;
-        $existingCardSIM = $user->profile->card_sim ?? null;
-        $existingCardNPWP = $user->profile->card_npwp ?? null;
-        $existingDocumentA = $user->profile->add_document_a ?? null;
-        $existingDocumentB = $user->profile->add_document_B ?? null;
-        $existingDocumentC = $user->profile->add_document_c ?? null;
-        $existingTransfer = $user->profile->transfer ?? null;
-        $existingCardFamily = $user->profile->card_family ?? null;
     
         if ($agent->isMobile()) {
-            return view('mobiles.register-profile', compact('user', 'existingAvatar', 'existingCardKTP', 'existingCardIjazah', 'existingCardSKCK', 'existingCardCertificate', 'existingCardSIM', 'existingCardNPWP', 'existingDocumentA', 'existingDocumentB', 'existingDocumentC', 'existingTransfer', 'existingCardFamily'));
+            return view('mobiles.register-profile', compact('user', 'existingAvatar'));
         } elseif ($agent->isDesktop()) {
-            return view('desktop.register-profile', compact('user', 'existingAvatar', 'existingCardKTP', 'existingCardIjazah', 'existingCardSKCK', 'existingCardCertificate', 'existingCardSIM', 'existingCardNPWP', 'existingDocumentA', 'existingDocumentB', 'existingDocumentC', 'existingTransfer', 'existingCardFamily'));
+            return view('desktop.register-profile', compact('user', 'existingAvatar'));
         } else {
             // Jika bukan perangkat mobile atau desktop, Anda bisa mengembalikan tampilan default di sini.
-            return view('desktop.register-profile', compact('user', 'existingAvatar', 'existingCardKTP', 'existingCardIjazah', 'existingCardSKCK', 'existingCardCertificate', 'existingCardSIM', 'existingCardNPWP', 'existingDocumentA', 'existingDocumentB', 'existingDocumentC', 'existingTransfer', 'existingCardFamily'));
+            return view('desktop.register-profile', compact('user', 'existingAvatar'));
         }
     }    
 
@@ -68,105 +57,13 @@ class ProfileController extends Controller
         $user = User::findOrFail(Auth::user()->id);
 
         $existingAvatar = $user->profile->avatar ?? null;
-        $existingCardKTP = $user->profile->card_ktp ?? null;
-        $existingCardIjazah = $user->profile->card_ijazah ?? null;
-        $existingCardSKCK = $user->profile->card_skck ?? null;
-        $existingCardCertificate = $user->profile->card_certificate ?? null;
-        $existingCardSIM = $user->profile->card_sim ?? null;
-        $existingCardNPWP = $user->profile->card_npwp ?? null;
-        $existingDocumentA = $user->profile->add_document_a ?? null;
-        $existingDocumentB = $user->profile->add_document_B ?? null;
-        $existingDocumentC = $user->profile->add_document_c ?? null;
-        $existingTransfer = $user->profile->transfer ?? null;
-        $existingCardFamily = $user->profile->card_family ?? null;
         
-
         $path1 = $existingAvatar;
         if ($request->hasFile('avatar')) {
             if ($existingAvatar && $existingAvatar !== 'default_avatar.jpg') {
                 Storage::delete($existingAvatar);
             }
             $path1 = $request->file('avatar')->store('public/avatars');
-        }
-
-        $path2 = $existingCardKTP;
-        if ($request->hasFile('card_ktp')) {
-            if ($existingCardKTP && $existingCardKTP !== 'default_avatar.jpg') {
-                Storage::delete($existingCardKTP);
-            }
-            $path2 = $request->file('card_ktp')->store('public/ktp');
-        }
-
-        $path3 = $existingCardIjazah;
-        if ($request->hasFile('card_ijazah')) {
-            if ($existingCardIjazah && $existingCardIjazah !== 'default_avatar.jpg') {
-                Storage::delete($existingCardIjazah);
-            }
-            $path3 = $request->file('card_ijazah')->store('public/ijazah');
-        }
-
-        $path4 = $existingCardSKCK;
-        if ($request->hasFile('card_skck')) {
-            if ($existingCardSKCK && $existingCardSKCK !== 'default_avatar.jpg') {
-                Storage::delete($existingCardSKCK);
-            }
-            $path4 = $request->file('card_skck')->store('public/skck');
-        }
-
-        $path5 = $existingCardCertificate;
-        if ($request->hasFile('card_certificate')) {
-            if ($existingCardCertificate && $existingCardCertificate !== 'default_avatar.jpg') {
-                Storage::delete($existingCardCertificate);
-            }
-            $path5 = $request->file('card_certificate')->store('public/certificate');
-        }
-
-        $path6 = $existingCardSIM;
-        if ($request->hasFile('card_sim')) {
-            if ($existingCardSIM && $existingCardSIM !== 'default_avatar.jpg') {
-                Storage::delete($existingCardSIM);
-            }
-            $path6 = $request->file('card_sim')->store('public/sim');
-        }
-
-        $path7 = $existingCardNPWP;
-        if ($request->hasFile('card_npwp')) {
-            if ($existingCardNPWP && $existingCardNPWP !== 'default_avatar.jpg') {
-                Storage::delete($existingCardNPWP);
-            }
-            $path7 = $request->file('card_npwp')->store('public/npwp');
-        }
-
-        $path8 = $existingDocumentA;
-        if ($request->hasFile('add_document_a')) {
-            if ($existingDocumentA && $existingDocumentA !== 'default_avatar.jpg') {
-                Storage::delete($existingDocumentA);
-            }
-            $path8 = $request->file('add_document_a')->store('public/add_documents');
-        }
-
-        $path9 = $existingDocumentB;
-        if ($request->hasFile('add_document_b')) {
-            if ($existingDocumentB && $existingDocumentB !== 'default_avatar.jpg') {
-                Storage::delete($existingDocumentB);
-            }
-            $path9 = $request->file('add_document_b')->store('public/add_documents');
-        }
-
-        $path10 = $existingDocumentC;
-        if ($request->hasFile('add_document_c')) {
-            if ($existingDocumentC && $existingDocumentC !== 'default_avatar.jpg') {
-                Storage::delete($existingDocumentC);
-            }
-            $path10 = $request->file('add_document_c')->store('public/add_documents');
-        }
-
-        $path11 = $existingCardFamily;
-        if ($request->hasFile('card_family')) {
-            if ($existingCardFamily && $existingCardFamily !== 'default_avatar.jpg') {
-                Storage::delete($existingCardFamily);
-            }
-            $path11 = $request->file('card_family')->store('public/card_families');
         }
 
         $user->profile()->updateOrCreate(
@@ -199,7 +96,7 @@ class ProfileController extends Controller
             ]
         );
 
-        return Redirect::to('/dashboard')->withInput();
+        return Redirect::to('documents')->withInput();
     }
 
     public function show(Profile $profile)
