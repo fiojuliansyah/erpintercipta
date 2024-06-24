0d3c4c90303ca009b2072d100e0c67827ec58d93
Fio Juliansyah <fiojuliansyah@gmail.com>
2024-06-24 11:00:44 +0700

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -38,39 +38,5 @@ class AppServiceProvider extends ServiceProvider
         });        
 
         Schema::defaultStringLength(191);
-
-        if (!$this->app->runningInConsole()) {
-    
-            $countPelamar = User::whereDoesntHave('roles')
-                ->whereHas('profile', function ($query) {
-                    $query->where('department', null);
-                })
-                ->whereDoesntHave('candidate')
-                ->count();
-        
-            view()->share('countPelamar', $countPelamar);
-
-            $countKandidat = Candidate::where('status', 0)->count();
-            view()->share('countKandidat', $countKandidat);
-
-            $countPending = Candidate::where('status', 1)->count();
-            view()->share('countPending', $countPending);
-
-            $countNCC = Candidate::where('status', 2)->count();
-            view()->share('countNCC', $countNCC);
-
-            $countGNC = Candidate::where('status', 3)->count();
-            view()->share('countGNC', $countGNC);
-
-            $countInterview = Candidate::where('status', 4)->count();
-            view()->share('countInterview', $countInterview);
-
-            $countReject = Candidate::where('status', 5)->count();
-            view()->share('countReject', $countReject);
-
-            Notification::extend('whatsapp', function ($app) {
-                return new WhatsappChannel();
-            });
-        }
     }
 }
